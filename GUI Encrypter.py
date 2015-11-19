5030e7acb4ade994d3c4699d867f68b84628ba95
Irides-Chromium <zhuhaoqing@live.cn>
2015-11-19 19:47:38 +0800

    Update GUI Encrypter
    This new version adds a full preference and another language support.

--- GUI Encrypter.py
+++ GUI Encrypter.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3.4
+# -*- coding: utf-8 -*-
 ### GUI Encrypter by Zhu Haoqing(Originally Zhangjingye) ###
 ### Rev: 2.0
 ### Runtime Environment: Python3
@@ -22,7 +24,7 @@
 #   showhelp        Help menu -> Help
 #   preference      Edit menu -> Preference
 
-
+import Lang
 import tkinter
 import tkinter.font
 import time
@@ -30,7 +32,7 @@ from math import floor
 from random import random
 from tkinter import ttk
 
-# -*- coding: utf-8 -*-
+
 
 def scale(cur, res, num):
 # Default Settings
@@ -88,37 +90,37 @@ def scale(cur, res, num):
     # To
     if res != 10:
         s = ''
-        for i in range(1, 17):
-            if int(res)**i > num:
-                n = i
-                break
-        a = 0
-        if outmode == 1:
-            for i in range(1, n+1):
-                a = num%int(res)
-                b = a
-                if a >= 10 and a <= 35:
-                    b = chr(int(a + 87))
-                if a >= 36 and a <= 61:
-                    b = chr(int(a + 29))
-                s = str(b) + s
-                num = int(floor(num/int(res)))
+    for i in range(1, 17):
+        if int(res)**i > num:
+            n = i
+        break
+    a = 0
+    if outmode == 1:
+        for i in range(1, n+1):
+            a = num%int(res)
+            b = a
+            if a >= 10 and a <= 35:
+                b = chr(int(a + 87))
+            if a >= 36 and a <= 61:
+                b = chr(int(a + 29))
+            s = str(b) + s
+            num = int(floor(num/int(res)))
             num = s
-        else:
-            for i in range(1, n+1):
-                a = num%int(res)
-                b = a
-                if a >= 10 and a <= 35:
-                    b = chr(int(a + 87))
-                s = str(b) + s
-                num = int(floor(num/int(res)))
+    else:
+        for i in range(1, n+1):
+            a = num%int(res)
+            b = a
+            if a >= 10 and a <= 35:
+                b = chr(int(a + 87))
+            s = str(b) + s
+            num = int(floor(num/int(res)))
             num = s
     if error == True:
         return 'ERROR'
     if error == False and Defined == True:
         if Positive == False:
             num = '-' + str(num)
-        return num
+    return num
 
 def count(string):
     if type(string) == type(2):
@@ -145,7 +147,7 @@ def reverse(string):
 def encrypt(*args):
     Text = TextBox.get('1.0', 'end')
     if Text == '' or Text == '\n' or Text == ' ':
-        Message.set('Don\'t play dicks.')
+        Message.set(liblang.Msg_ERR)
     uText = Text.encode('utf-8')
     bak = Text
     jmh = encrypter(Text)
@@ -154,15 +156,15 @@ def encrypt(*args):
     ol = decrypter(Text, True)
     dct = ol
     if dct != bak:
-        TextStatus.set('Encryption failed.')
+        TextStatus.set(liblang.Msg_Stat_Enc[0])
     else:
         cleartext()
-        TextStatus.set('Encryption succeeded.')
-        TimeUsed.set(str(needtime) + 'ms')
+        TextStatus.set(liblang.Msg_Stat_Enc[1])
+        TimeUsed.set(str(needtime) + liblang.Time_Encryption)
         TextBox.insert('1.0', Text)
 
 def encrypter(string):
-    try:
+##    try:
         ret = '~'
         d = str(int(time.time()*1000))
         part2 = d[10:13]
@@ -187,26 +189,26 @@ def encrypter(string):
         lastTime = int(time.time()*1000)
         TimeUsed = int(lastTime) - int(d)
         return ret, TimeUsed
-    except:
-        e = Exception()
-        return e
+##    except:
+##        e = Exception()
+##        return e
 
 def decrypt(*args):
     Text = TextBox.get('1.0', 'end')
     if Text == '' or Text == '\n' or Text == ' ' or Text[0] != '~':
-        Message.set('Don\'t play dicks.')
+        Message.set(liblang.Msg_ERR)
     Text = decrypter(Text, False)
     if type(Text) == type(('2',)):
         cleartext()
         TextBox.insert('1.0', Text[0])
         TextStatus.set(Text[1])
         Message.set(Text[2])
-        TimeUsed.set(str(Text[3]) + 'ms') 
+        TimeUsed.set(str(Text[3]) + liblang.Time_Encryption) 
     else:
-        TextStatus.set('Decryption Failed')
+        TextStatus.set(liblang.Msg_Stat_Dec[0])
 
 def decrypter(string, checksum):
-    try:
+##    try:
         fT = int(time.time()*1000)
         string = string[1:-1]
         string = string.split('!')
@@ -226,13 +228,10 @@ def decrypter(string, checksum):
             ectime = str(part7) + str(part9) + str(part2)
             tc = time.ctime(float(ectime[0:10])).split(' ')
             date = tc[4] + ' ' + tc[1] + ' ' + tc[2] + ' ' + tc[3]
-            texts = 'Decryption succeed.'
-            msg = 'Encrypted on: ' + date
+            texts = liblang.Msg_Stat_Dec[1]
+            msg = liblang.Time_Encrypted + date
             retn = part4, texts, msg, needtime
         return retn
-    except:
-        e = Exception()
-        return e
 
 def uc3(this, utc, it):
     retn = ''
@@ -258,82 +257,133 @@ def un3(this, utc, it):
             retn += chr(int(int(tmmp[i], 25) - utc + int(i)))
         else:
             retn += chr(int(tmmp[i], 26) + int(it) - i)
-    retn
     return retn
-
-def ABOUT():
-    '''This is a program which is used to encrypt and decrypt strings written by Zhu Haoqing. The Program is originally written by zhangjingye (Github: zhangjingye03) in JavaScript, translated by zhuhaoqing (Github: Irides-Chromium) into Python3.
-CopyRight (C) 2015.10.1-31
-All Rights Reserved.
-
-Author: <zjy@sugus>
-Translater: <zhq>
-Version 2.0
-
-This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-
-This program is written by:
-Zhang Jingye.
-And translated by:
-Zhu Haoqing
-'''
 
 def about(*args):
     cleartext()
-    TextBox.insert('1.0', ABOUT.__doc__)
+    TextBox.insert('1.0', liblang.ABOUT.__doc__)
     
 def Quit(*args):
-    try: win.destroy()
-    except: root.destroy() 
+    win.destroy()
+    option.destroy()
+    root.destroy()
 
 def selectall():
     TextBox.tag_add("sel", "1.0", "end-1c")
     TextBox.mark_set("insert", "1.0")
     TextBox.see("insert")
 
-def HELP():
-    '''This is the Help of the Encrypter. Enter text or cipher into the Text Box and encrypt or decrypt. Press Escape to close the program. Press F1 to show this help.
-'''
-
 def showhelp(*args):
     win = tkinter.Toplevel(root)
-    win.title('Help')
+    win.title(liblang.Menu_Help)
     win.resizable(False, False)
 
     helpframe = ttk.Frame(win, padding='3 3 12 12')
     helpframe.grid(column=0, row=0)
     global helptext
     helptext = tkinter.StringVar()
-    y = win.winfo_screenheight()/2 - 100
-    x = win.winfo_screenwidth()/2 - 140
+    y = (win.winfo_screenheight() - 25)/2 - 50
+    x = (win.winfo_screenwidth() - 65)/2 - 140
     win.geometry("275x100+%d+%d" % (x, y))
     ttk.Label(helpframe, width=35, textvariable=helptext, font=ubuntu, wrap=260).grid(column=0, row=0, rowspan=5, sticky='NSWE')
-    helptext.set(HELP.__doc__)
+    helptext.set(liblang.HELP.__doc__)
 
-def preference(*args):
-    option = tkinter.Toplevel(root)
-    option.title('Preference')
-    option.resizable(False, False)
+class Default():
+    lang = 'en_US'
+    mode = 'normal'
 
-    y = option.winfo_screenheight()/2 - 100
-    x = option.winfo_screenwidth()/2 - 140
-    option.geometry("300x250+%d+%d" % (x, y))
-    
-    optionframe = ttk.Frame(option, padding='3 3 12 12')
-    optionframe.grid(column=0, row=0)
-    global note
-    note = tkinter.StringVar()
+class Settings():
 
-    Note = ttk.Label(optionframe, width=35, textvariable=note, font=uMono, wrap=260)
-    Note.grid(column=0, row=0, sticky='NSWE')
-    Note.grid_configure(padx=10, pady=10)
-    note.set('There is no other options yet, Strict mode and hex mode will be added in future updates and branches.')
+    def __init__(self):
+        try:
+            file = open('options')
+            lines = file.read()
+            line = lines.splitlines()
+            settings = {}
+            for i in line:
+                j = i.split('=')
+                settings[j[0]] = j[1]
+            file.close()
+            
+            global lang, mode
+            lang = settings.get('Language')
+            mode = settings.get('Mode')
+            
+            global liblang
+            if lang == 'en_US':
+                from Lang import en_US as liblang
+            else:
+                from Lang import zh_CN as liblang
+        except:
+            file = open('options', 'w')
+            lang = 'Language' + '=' + Default.lang
+            mode = 'Mode' + '=' + Default.mode
+            file.write('\n'.join((lang, mode)))
+            file.close()
+            del lang, mode
+            
+            global lang, mode
+            lang = 'en_US'
+            mode = 'Normal'
+
+    def SettingDialog():
+        option = tkinter.Toplevel(root)
+        option.title(liblang.Menu_Edit_[4])
+        option.resizable(False, False)
+
+        y = (option.winfo_screenheight() - 25)/2 - 100
+        x = (option.winfo_screenwidth() - 65)/2 - 120
+        option.geometry("235x195+%d+%d" % (x, y))
+        
+        optionframe = ttk.Frame(option, padding='3 3 12 12')
+        optionframe.grid(column=0, row=0)
+
+        global chosen1, chosen2
+        chosen1 = tkinter.StringVar()
+        chosen2 = tkinter.StringVar()
+        
+        langframe = tkinter.LabelFrame(optionframe, text='Language', font=ubuntu)
+        langframe.grid(column=0, row=1, columnspan=2, sticky='NSWE')
+        eng = tkinter.Radiobutton(langframe, text='English', font=ubuntu, variable=chosen1, value='en_US')
+        eng.grid(column=1, row=2, sticky='WN')
+        chn = tkinter.Radiobutton(langframe, text='中文', font=ubuntu, variable=chosen1, value='zh_CN')
+        chn.grid(column=2, row=2, sticky='WN')
+        tkinter.Label(langframe, text='Note: You should restart the program to apply the lang change.', font=ubuntus, wrap=220, anchor='w', justify='left').grid(column=1, row=3, columnspan=2, sticky='WN')
+
+        modeframe = tkinter.LabelFrame(optionframe, text='Mode', font=ubuntu)
+        modeframe.grid(column=0, row=2, columnspan=2, sticky='NSWE')
+        nrm = tkinter.Radiobutton(modeframe, text='Normal', font=ubuntu, variable=chosen2, value='Normal')
+        nrm.grid(column=1, row=4, sticky='WN')
+        hexmode = tkinter.Radiobutton(modeframe, text='Hex mode', font=ubuntu, variable=chosen2, value='Hex')
+        hexmode.grid(column=2, row=4, sticky='EN')
+
+        if lang == 'en_US':eng.select()
+        else:chn.select()
+        if mode == 'Normal':nrm.select()
+        else:hexmode.select()
+        
+        tkinter.Label(optionframe).grid(column=0, row=3)
+        tkinter.Button(optionframe, text='Apply', font=ubuntus, command=Settings.applyset).grid(column=0, row=4, sticky='WS')
+        tkinter.Button(optionframe, text='Close', font=ubuntus, command=option.destroy).grid(column=1, row=4, sticky='ES')
+        
+    def applyset():
+
+        lang = chosen1.get()
+        mode = chosen2.get()
+        file = open('options', 'w')
+        lang = 'Language' + '=' + lang
+        mode = 'Mode' + '=' + mode
+        file.write('\n'.join((lang, mode)))
+        file.close()
+##        del chosen1, chosen2 XXX
+        Settings()
+
+Settings()
+
 
 ### Basic Window ###
 root = tkinter.Tk()
-root.title("Encrypter")
+root.title(liblang.Title)
 root.resizable(False, False)
 
 y = root.winfo_screenheight()/2 - 250
@@ -351,24 +401,25 @@ root.option_add('*tearOff', False)
 menubar = tkinter.Menu(root)
 
 edit = tkinter.Menu(menubar, tearoff=0)
-edit.add_command(label='Copy')
-edit.add_command(label='Cut')
-edit.add_command(label='Paste')
-edit.add_command(label='Select all')
+edit.add_command(label=liblang.Menu_Edit_[0])
+edit.add_command(label=liblang.Menu_Edit_[1])
+edit.add_command(label=liblang.Menu_Edit_[2])
+edit.add_command(label=liblang.Menu_Edit_[3])
 edit.add_separator()
-edit.add_command(label='Preference')
 
-edit.entryconfigure("Cut", command=lambda: root.event_generate("<Control-x>"))
-edit.entryconfigure("Copy", command=lambda: root.event_generate("<Control-c>"))
-edit.entryconfigure("Paste", command=lambda: root.event_generate("<Control-v>"))
-edit.entryconfigure("Select all", command=selectall)
-edit.entryconfigure("Preference", command=preference)
-menubar.add_cascade(label='Edit', menu=edit)
+edit.add_command(label=liblang.Menu_Edit_[4])
+
+edit.entryconfigure(liblang.Menu_Edit_[0], command=lambda: root.event_generate("<Control-x>"))
+edit.entryconfigure(liblang.Menu_Edit_[1], command=lambda: root.event_generate("<Control-c>"))
+edit.entryconfigure(liblang.Menu_Edit_[2], command=lambda: root.event_generate("<Control-v>"))
+edit.entryconfigure(liblang.Menu_Edit_[3], command=selectall)
+edit.entryconfigure(liblang.Menu_Edit_[4], command=Settings.SettingDialog)
+menubar.add_cascade(label=liblang.Menu_Edit, menu=edit)
 
 Help = tkinter.Menu(menubar, tearoff=0)
-Help.add_command(label='Help', command=showhelp)
-Help.add_command(label='About', command=about)
-menubar.add_cascade(label='Help', menu=Help)
+Help.add_command(label=liblang.Menu_Help, command=showhelp)
+Help.add_command(label=liblang.Lbl_Btn[3], command=about)
+menubar.add_cascade(label=liblang.Menu_Help, menu=Help)
 
 root.config(menu=menubar)
 
@@ -378,6 +429,7 @@ TextStatus = tkinter.StringVar()
 Message = tkinter.StringVar()
 
 ### Font ###
+ubuntus = tkinter.font.Font(family='Ubuntu', size=10)
 ubuntu = tkinter.font.Font(family='Ubuntu', size=12)
 uMono = tkinter.font.Font(family='Ubuntu Mono', size=13)
 
@@ -393,18 +445,18 @@ s.grid(column=5, row=2, sticky='NS')
 TextBox['yscrollcommand'] = s.set
 
 ### Button ###
-ttk.Button(mainframe, width=10, text="Encrypt", command=encrypt).grid(column=1, row=6, sticky='NWSE')
-ttk.Button(mainframe, width=10, text="Decrypt", command=decrypt).grid(column=2, row=6, sticky='NWSE')
-ttk.Button(mainframe, width=10, text="Clear", command=cleartext).grid(column=3, row=6, sticky='NWSE')
-ttk.Button(mainframe, width=10, text="About", command=about).grid(column=4, row=6, sticky='NWSE')
+tkinter.Button(mainframe, width=8, text=liblang.Lbl_Btn[0], font=ubuntus, command=encrypt).grid(column=1, row=6, sticky='NWSE')
+tkinter.Button(mainframe, width=8, text=liblang.Lbl_Btn[1], font=ubuntus, command=decrypt).grid(column=2, row=6, sticky='NWSE')
+tkinter.Button(mainframe, width=8, text=liblang.Lbl_Btn[2], font=ubuntus, command=cleartext).grid(column=3, row=6, sticky='NWSE')
+tkinter.Button(mainframe, width=8, text=liblang.Lbl_Btn[3], font=ubuntus, command=about).grid(column=4, row=6, sticky='NWSE')
 
 ### Labeling ###
-ttk.Label(mainframe, text="Text Box", font=ubuntu).grid(row=1, column=1, sticky='NW')
-ttk.Label(mainframe, text="Status", font=uMono).grid(column=1, row=3, sticky='SW')
+ttk.Label(mainframe, text=liblang.Lbl_Label[0], font=ubuntu).grid(row=1, column=1, sticky='NW')
+ttk.Label(mainframe, text=liblang.Lbl_Label[1], font=uMono).grid(column=1, row=3, sticky='SW')
 ttk.Label(mainframe, textvariable=TextStatus, font=uMono).grid(column=2, row=3, columnspan=3, sticky='NE')
-ttk.Label(mainframe, text="Message", font=uMono).grid(column=1, row=4, sticky='SW')
+ttk.Label(mainframe, text=liblang.Lbl_Label[2], font=uMono).grid(column=1, row=4, sticky='SW')
 ttk.Label(mainframe, textvariable=Message, font=uMono).grid(column=2, row=4, columnspan=3, sticky='NE')
-ttk.Label(mainframe, text="Time Used", font=uMono).grid(column=1, row=5, sticky='NW')
+ttk.Label(mainframe, text=liblang.Lbl_Label[3], font=uMono).grid(column=1, row=5, sticky='NW')
 ttk.Label(mainframe, textvariable=TimeUsed, font=uMono).grid(column=3, row=5, columnspan=2, sticky='NE')
 
 ### Padding ###
