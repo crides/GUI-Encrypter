4f71a653bdd66d139a9bc0d939bfbf1833fee81a
Irides-Chromium <zhuhaoqing@live.cn>
2015-11-25 10:56:49 +0800

    Update Library.py

--- Library.py
+++ Library.py
@@ -1,3 +1,6 @@
+import time
+from math import floor
+from random import random
 
 def scale(cur, res, num):
 # Default Settings
@@ -55,37 +58,37 @@ def scale(cur, res, num):
     # To
     if res != 10:
         s = ''
-    for i in range(1, 17):
-        if int(res)**i > num:
-            n = i
-        break
-    a = 0
-    if outmode == 1:
-        for i in range(1, n+1):
-            a = num%int(res)
-            b = a
-            if a >= 10 and a <= 35:
-                b = chr(int(a + 87))
-            if a >= 36 and a <= 61:
-                b = chr(int(a + 29))
-            s = str(b) + s
-            num = int(floor(num/int(res)))
+        for i in range(1, 17):
+            if int(res)**i > num:
+                n = i
+                break
+        a = 0
+        if outmode == 1:
+            for i in range(1, n+1):
+                a = num%int(res)
+                b = a
+                if a >= 10 and a <= 35:
+                    b = chr(int(a + 87))
+                if a >= 36 and a <= 61:
+                    b = chr(int(a + 29))
+                s = str(b) + s
+                num = int(floor(num/int(res)))
             num = s
-    else:
-        for i in range(1, n+1):
-            a = num%int(res)
-            b = a
-            if a >= 10 and a <= 35:
-                b = chr(int(a + 87))
-            s = str(b) + s
-            num = int(floor(num/int(res)))
+        else:
+            for i in range(1, n+1):
+                a = num%int(res)
+                b = a
+                if a >= 10 and a <= 35:
+                    b = chr(int(a + 87))
+                s = str(b) + s
+                num = int(floor(num/int(res)))
             num = s
     if error == True:
         return 'ERROR'
     if error == False and Defined == True:
         if Positive == False:
             num = '-' + str(num)
-    return num
+        return num
 
 def count(string):
     if type(string) == type(2):
@@ -95,13 +98,6 @@ def count(string):
         count += 1
     return int(count)
 
-def cleartext():
-    TextStatus.set('')
-    Message.set('')
-    TimeUsed.set('')
-    TextBox.delete('1.0', 'end')
-
-
 def reverse(string):
     string = str(string)
     l = list(string)
@@ -109,41 +105,22 @@ def reverse(string):
     s = ''.join(l)
     return s
 
-def encrypt(*args):
-    Text = TextBox.get('1.0', 'end')
-    if Text == '' or Text == '\n' or Text == ' ':
-        Message.set(liblang.Msg_ERR)
-    uText = Text.encode('utf-8')
-    bak = Text
-    jmh = encrypter(Text)
-    Text = jmh[0]
-    needtime = jmh[1]
-    ol = decrypter(Text, True)
-    dct = ol
-    if dct != bak:
-        TextStatus.set(liblang.Msg_Stat_Enc[0])
-    else:
-        cleartext()
-        TextStatus.set(liblang.Msg_Stat_Enc[1])
-        TimeUsed.set(str(needtime) + liblang.Time_Encryption)
-        TextBox.insert('1.0', Text)
-
 def encrypter(string):
 ##    try:
         ret = '~'
         d = str(int(time.time()*1000))
         part2 = d[10:13]
-        p2 = scale(10, 25, reverse(part2))
+        p2 = scale(10, 15, reverse(part2))
         part4 = string;
         part6 = int(round(random()*100))
         p6 = scale(10, 3, part6)
         part7 = d[0:5]
-        p7 = reverse(scale(10, 36, (int(part7) + 25 - int(p6))))
+        p7 = reverse(scale(10, 36, (int(part7) + 15 - int(p6))))
         part8 = int(round(random()*10));
         if part8 == 0: part8 = 10
         p8 = scale(10, 9, part8)
         part9 = d[5:10]
-        p9 = reverse(scale(10, 35, (int(part9) - 25*part8)))
+        p9 = reverse(scale(10, 35, (int(part9) - 15*part8)))
         p4 = uc3(part4, part2, part7)
         ret += p2 +'!'
         ret += p4 +'!'
@@ -158,32 +135,18 @@ def encrypter(string):
 ##        e = Exception()
 ##        return e
 
-def decrypt(*args):
-    Text = TextBox.get('1.0', 'end')
-    if Text == '' or Text == '\n' or Text == ' ' or Text[0] != '~':
-        Message.set(liblang.Msg_ERR)
-    Text = decrypter(Text, False)
-    if type(Text) == type(('2',)):
-        cleartext()
-        TextBox.insert('1.0', Text[0])
-        TextStatus.set(Text[1])
-        Message.set(Text[2])
-        TimeUsed.set(str(Text[3]) + liblang.Time_Encryption) 
-    else:
-        TextStatus.set(liblang.Msg_Stat_Dec[0])
-
 def decrypter(string, checksum):
 ##    try:
         fT = int(time.time()*1000)
         string = string[1:-1]
         string = string.split('!')
-        part2 = reverse(int(string[0], 25))
+        part2 = reverse(int(string[0], 15))
         part69 = string[2].split(',');
         part6 = int(part69[0], 3)
-        part7 = int(reverse(part69[1]), 36) - 25 + int(part69[0])
+        part7 = int(reverse(part69[1]), 36) - 15 + int(part69[0])
         part8 = int(part69[2])
         part9 = reverse(part69[3])
-        part9 = int(part9, 35) + (25*part8)
+        part9 = int(part9, 35) + (15*part8)
         part4 = string[1]
         part4 = un3(part4, part2, part7);
         lT = int(time.time()*1000)
@@ -193,9 +156,7 @@ def decrypter(string, checksum):
             ectime = str(part7) + str(part9) + str(part2)
             tc = time.ctime(float(ectime[0:10])).split(' ')
             date = tc[4] + ' ' + tc[1] + ' ' + tc[2] + ' ' + tc[3]
-            texts = liblang.Msg_Stat_Dec[1]
-            msg = liblang.Time_Encrypted + date
-            retn = part4, texts, msg, needtime
+            retn = part4, date, needtime
         return retn
 
 def uc3(this, utc, it):
@@ -205,9 +166,9 @@ def uc3(this, utc, it):
     n = count(this)
     for i in range(n):
         if i%2 != 0:
-            retn += str(scale(10, 25, str(ord(this[i]) + int(utc) - i))) + ','
+            retn += str(scale(10, 15, str(ord(this[i]) + int(utc) - int(i)))) + ','
         else:
-            retn += str(scale(10, 26, str(ord(this[i]) - int(it) + int(i)))) + ','
+            retn += str(scale(10, 12, str(ord(this[i]) - int(it) + int(i)))) + ','
 
     retn = retn[0:-1];
     return retn
@@ -219,39 +180,10 @@ def un3(this, utc, it):
     tmmp = this.split(',')
     for i in range(count(tmmp)):
         if i%2 != 0:
-            retn += chr(int(int(tmmp[i], 25) - utc + int(i)))
+            retn += chr(int(int(tmmp[i], 15) - utc + int(i)))
         else:
-            retn += chr(int(tmmp[i], 26) + int(it) - i)
+            retn += chr(int(tmmp[i], 12) + int(it) - i)
     return retn
-
-def about(*args):
-    cleartext()
-    TextBox.insert('1.0', liblang.ABOUT.__doc__)
-    
-def Quit(*args):
-    win.destroy()
-    option.destroy()
-    root.destroy()
-
-def selectall():
-    TextBox.tag_add("sel", "1.0", "end-1c")
-    TextBox.mark_set("insert", "1.0")
-    TextBox.see("insert")
-
-def showhelp(*args):
-    win = tkinter.Toplevel(root)
-    win.title(liblang.Menu_Help)
-    win.resizable(False, False)
-
-    helpframe = ttk.Frame(win, padding='3 3 12 12')
-    helpframe.grid(column=0, row=0)
-    global helptext
-    helptext = tkinter.StringVar()
-    y = (win.winfo_screenheight() - 25)/2 - 50
-    x = (win.winfo_screenwidth() - 65)/2 - 140
-    win.geometry("275x100+%d+%d" % (x, y))
-    ttk.Label(helpframe, width=35, textvariable=helptext, font=ubuntu, wrap=260).grid(column=0, row=0, rowspan=5, sticky='NSWE')
-    helptext.set(liblang.HELP.__doc__)
 
 class Default():
     lang = 'en_US'
