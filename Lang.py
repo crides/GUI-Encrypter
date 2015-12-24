8e689cd6d1361c8886a7001d2aa919e5e995a92b
Irides-Chromium <zhuhaoqing@live.cn>
2015-12-24 19:40:36 +0800

    Update Lang.py

--- Lang.py
+++ Lang.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3.4
 
 class en_US():
-    Title = 'Encrypter' 
+    Title = 'GEncrypter' 
     
     notification = 'Program Started!'
     
-    Msg_ERR = 'Don\'t play dicks'
+    Msg_ERR = 'There is an Error.'
     Msg_Stat_Enc = ['Encryption Failed.', 'Encryption Succeeded.']
     Msg_Stat_Dec = ['Decryption Failed.', 'Decryption Succeeded.']
     
@@ -24,12 +24,12 @@ class en_US():
     
     def ABOUT():
         '''This is a program which is used to encrypt and decrypt strings written by Zhu Haoqing. The Program is originally written by zhangjingye (Github: zhangjingye03) in JavaScript, translated by zhuhaoqing (Github: Irides-Chromium) into Python3. The Program can only decrypt strings which were encrypted by (any branches, or the original web version by Zhang Jingye) this program.
-CopyRight (C) 2015.10.1-31
+CopyRight (C) 2014 ~ 2016
 All Rights Reserved.
 
 Author: <zjy@sugus>
 Translater: <zhuhaoqing@live.cn>
-Version 3.0
+Version 4.0
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -58,7 +58,7 @@ class zh_CN():
     
     notification = '程序启动！'
     
-    Msg_ERR = '不要耍吊'
+    Msg_ERR = '程序出错'
     Msg_Stat_Enc = ['加密失败', '加密成功']
     Msg_Stat_Dec = ['解密失败', '解密成功']
     
@@ -76,10 +76,10 @@ class zh_CN():
     Abt_Comment = '加密你的字符串'
     def ABOUT():
         '''这是一个由 Zhu Haoqing 编写的用于加密和解密字符串的程序。这个程序原本由 zhangjingye (Github: zhangjingye03) 用JavaScript编写，由 Zhu Haoqing (Github: Irides-Chromium) 翻译成为 Python3。本程序只能解密由(任意分支或原网页版的)本程序加密的字符串。
-版权所有(C) 2015.10.1-31
+版权所有(C) 2014 ~ 2016
 作者： <zjy@sugus>
 翻译： <zhuhaoqing@live.cn>
-版本： 3.0
+版本： 4.0
 
 本程序是免费的软件；您可以在自由软件基金会(即Free Software Foundation)发行的通用公共许可证 (GNU General Public Lisence) 的条件下重新发行和/或更改它；可以是版本2.0，或者(按您的选择)任何之后的版本。
 本程序是为了希望它有用而发布的，但没有任何担保；甚至没有对(它的)可销售性或特定目的的适应性。详见GNU General Public License。
